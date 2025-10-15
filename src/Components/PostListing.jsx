c04b82bc25709adc9fd447662e93001443089c1c
Palden D. Titung <paldendorjetitung@gmail.com>
2025-10-15 22:41:45 +0545

    added the animation in post cards

--- src/Components/PostListing.jsx
+++ src/Components/PostListing.jsx
@@ -1,15 +1,30 @@
 import React, { useContext } from "react";
 import PostCard from "./PostCard";
 import { AppContext } from "../Context/AppContext";
+import { motion } from "framer-motion";
 
 const PostListing = () => {
   const { posts } = useContext(AppContext);
+
   return (
-    <div className="w-full p-4 flex flex-col gap-4">
-      {posts.map((post) => (
-        <PostCard post={post} key={post.id} />
-      ))}
-    </div>
+    <section>
+      <div className="w-full p-4 flex flex-col gap-4">
+        {posts?.map((post) => (
+          <motion.div
+            key={post.id}
+            initial={{ opacity: 0, y: -10 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+              duration: 0.4,
+              delay: post.id * 0.2,
+              ease: "easeOut",
+            }}
+          >
+            <PostCard post={post} />
+          </motion.div>
+        ))}
+      </div>
+    </section>
   );
 };
 
