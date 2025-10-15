d0488bcc1901158ecce8bc30930051e9dbafe7af
Palden D. Titung <paldendorjetitung@gmail.com>
2025-10-15 22:56:56 +0545

    improve the posts

--- src/Components/PostListing.jsx
+++ src/Components/PostListing.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import PostCard from "./PostCard";
 import { AppContext } from "../Context/AppContext";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 const PostListing = () => {
   const { posts } = useContext(AppContext);
@@ -9,20 +9,23 @@ const PostListing = () => {
   return (
     <section>
       <div className="w-full p-4 flex flex-col gap-4">
-        {posts?.map((post) => (
-          <motion.div
-            key={post.id}
-            initial={{ opacity: 0, y: -10 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{
-              duration: 0.4,
-              delay: post.id * 0.2,
-              ease: "easeOut",
-            }}
-          >
-            <PostCard post={post} />
-          </motion.div>
-        ))}
+        <AnimatePresence>
+          {posts?.map((post, index) => (
+            <motion.div
+              key={post.id}
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20 }}
+              transition={{
+                duration: 0.4,
+                delay: index * 0.05,
+                ease: "easeOut",
+              }}
+            >
+              <PostCard post={post} />
+            </motion.div>
+          ))}
+        </AnimatePresence>
       </div>
     </section>
   );
